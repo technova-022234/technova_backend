8df20b898e1f49150f205c75ec788f1b3fd1dc02
madhumitha3147 <pamminajahnavi@gmail.com>
2025-02-22 16:52:06 +0530

    loginpagebackend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connet from "./db.js"
 import cors from "cors"
+import User from "./models/loginschema.js"
 
 
 dotenv.config()
@@ -14,15 +15,31 @@ connet()
 app.use(express.json())
 app.use(cors(corsoptions))
 
-app.get("/info",(req,res) =>{
-    const a= 4+9
-    res.json({addition : a})
-})
-app.post("/addition",(req,res)=>{
-    const {a,b}=req.body;
-    const c= a+b
-    res.json({addition:c})
-})
+
+const verifyUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if user exists and password matches
+        const user = await User.findOne({ email, password });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        // If user exists and password matches, send success response
+        res.status(200).json({
+            message: "Sign-in successful",
+            user: { email: user.email},
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Error occurred", error: error.message });
+    }
+};
+
+// API Route for Login
+app.post("/api/users/login", verifyUser);
+
+
 app.listen(5000,()=> {
     console.log("server is running on port 5000")
 })
